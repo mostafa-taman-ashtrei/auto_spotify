f0e55283c1b80976aa80d94c3c4cce9800fc7703
NEO <neo.one.mail@gmail.com>
2021-04-05 16:45:17 +0200

    add songs to spotify

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
+import os
+from dotenv import load_dotenv
+
 from youtube import YoutubeClient
 from spotify import SpotifyClient
+
+load_dotenv()
 
 
 def main():
     youtube_client = YoutubeClient()
-    spotify_client = SpotifyClient()
+    spotify_client = SpotifyClient(os.getenv('SPOTFY_TOKEN'))
 
     playlists = youtube_client.get_playlists()
 
     music_playlist = [
-        playlist for playlist in playlists if playlist['name'] == 'music'
+        playlist for playlist in playlists if playlist['name'] == 'spotify'
     ]
     music_playlist_id = music_playlist[0]['id']
     my_songs = youtube_client.get_videos(music_playlist_id)
+
+    spotify_playlist_id = '3I5aRuFgAvZ4eiELHV11Zo'
+    existing_songs = spotify_client.song_exists(spotify_playlist_id)
+
+    for song in my_songs:
+        song_uri = spotify_client.search(song['artist'], song['track'])
+
+        if song_uri:
+            if song_uri in existing_songs:
+                print(f'{song_uri} already exists')
+            else:
+                added_song = spotify_client.add_Song(
+                    spotify_playlist_id, song_uri)
+                print(added_song)
+                if added_song:
+                    print(f'{song_uri} has been added to your playlist')
 
 
 if __name__ == '__main__':
--- spotify.py
+++ spotify.py
@@ -1,3 +1,52 @@
+import requests
+
+
 class SpotifyClient:
-    def __init__(self):
-        pass
+    def __init__(self, token):
+        self.token = token
+
+    def search(self, artist, track):
+        url = f'https://api.spotify.com/v1/search?query=track%3A{track}+artist%3A{artist}&type=track&offset=0&limit=20'
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.token}'
+        }
+
+        res = requests.get(url, headers=headers)
+        res_json = res.json()
+        print(res.status_code)
+        data = res_json['tracks']['items']
+
+        if data:
+            return data[0]['uri']
+        else:
+            raise Exception(f'No song found for {artist} named {track}')
+
+    def song_exists(self, playlist_id):
+        url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.token}'
+        }
+
+        res = requests.get(url, headers=headers)
+        res_json = res.json()
+        existing_songs = []
+
+        for x in res_json['items']:
+            existing_songs.append(x['track']['uri'])
+
+        return existing_songs
+
+    def add_Song(self, playlist_id, song_uri):
+        url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks?uris={song_uri}'
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.token}'
+        }
+
+        res = requests.post(url, headers=headers)
+        print(res.json())
+        return res.ok
